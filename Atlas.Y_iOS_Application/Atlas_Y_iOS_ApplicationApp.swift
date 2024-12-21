41f1c291b7013aa89e282207d03c0f451ef38053
Jishen Lin <minmuslin@outlook.com>
2024-12-21 16:32:13 +0800

    +

---
+++ Atlas.Y_iOS_Application/Atlas_Y_iOS_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atlas_Y_iOS_ApplicationApp.swift
+//  Atlas.Y_iOS_Application
+//
+//  Created by JishenLin on 2024/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Atlas_Y_iOS_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
